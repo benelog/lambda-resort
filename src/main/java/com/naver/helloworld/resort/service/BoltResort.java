47ceda00e027df0d59e1afb239a1f2bcb33336c0
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2015-01-06 13:58:40 +0900

    changed to sortBy method in the example of Bolt

--- src/main/java/com/naver/helloworld/resort/service/BoltResort.java
+++ src/main/java/com/naver/helloworld/resort/service/BoltResort.java
@@ -1,10 +1,8 @@
 package com.naver.helloworld.resort.service;
 
-import java.util.Comparator;
 import java.util.List;
 
 import ru.yandex.bolts.collection.Cf;
-import ru.yandex.bolts.collection.ListF;
 import ru.yandex.bolts.function.Function;
 import ru.yandex.bolts.function.Function1B;
 
@@ -19,27 +17,21 @@ public class BoltResort implements ResortService {
 
 	public List<String> findGuestNamesOfCompany(final String company) {
 		List<Guest> all = repository.findAll();
-		// filter
-		ListF<Guest> filtered = Cf.list(all)
+		return Cf.list(all)
 			.filter(new Function1B<Guest>() {
 				public boolean apply(Guest g) {
 					return company.equals(g.getCompany());
 				}
-			});
-		
-		// sort
-		filtered.sort(new Comparator<Guest>() {
-			public int compare(Guest o1, Guest o2) {
-				return Integer.compare(o1.getGrade(), o2.getGrade());
-			}
+			})
+			.sortBy(new Function<Guest, Integer>() {
+				public Integer apply(Guest g) {
+					return g.getGrade();
+				}
+			})
+			.map(new Function<Guest, String>() {
+				public String apply(Guest g) {
+					return g.getName();
+				}
 		});
-		
-		// map
-		ListF<String> names = filtered.map(new Function<Guest, String>() {
-			public String apply(Guest g) {
-				return g.getName();
-			}
-		});
-		return names.toList();
 	}
 }
