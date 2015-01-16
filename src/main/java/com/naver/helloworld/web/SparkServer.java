675a702d75fc02dad401d71784fce02b40f5f75e
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2015-01-17 06:22:44 +0900

    removed unnecessary imports

--- src/main/java/com/naver/helloworld/web/SparkServer.java
+++ src/main/java/com/naver/helloworld/web/SparkServer.java
@@ -2,15 +2,6 @@ package com.naver.helloworld.web;
 
 import static spark.Spark.*;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import com.naver.helloworld.resort.service.ResortService;
-
 public class SparkServer {
 	public static void main(String[] args) {
 		get("/guest/company/:company", (request, response) -> {
